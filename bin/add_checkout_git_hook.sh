80cd510e1c1afea0cb3cfd09fe28ade10743be4a
Joel Ryden <jryden@atlassian.com>
2018-11-15 22:43:53 +1100

    Fixing checkout hook

--- bin/add_checkout_git_hook.sh
+++ bin/add_checkout_git_hook.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+root_folder=$(git rev-parse --show-toplevel)
+command="$(npm bin)/floobits-workspace --share -w \`basename -s .git \\\`git config --get remote.origin.url\\\`\`-\`git rev-parse --abbrev-ref HEAD\` --no-browser"
 
-command="./bin/workspace --share -w \`basename -s .git \\\`git config --get remote.origin.url\\\`\`-\`git rev-parse --abbrev-ref HEAD\` --no-browser"
-
-if [ $# -ge 1 ]; then
+if [[ $# -ge 1 ]]; then
     command="$command -o $1"
 fi
 
-echo $command > .git/hooks/post-checkout
+mkdir -p ${root_folder}/.git/hooks/ && touch ${root_folder}/.git/hooks/post-checkout
+
+chmod +x ${root_folder}/.git/hooks/post-checkout
+echo ${command} > ${root_folder}/.git/hooks/post-checkout
